4e1deee40bb09fc6abc3271d50154ff9dbf33baf
Matthew Kaiser <mpkaise@outlook.com>
2020-10-30 17:41:40 -0400

    Fixel Local Path problem

--- ImageOutputting/ImageOutputting/outputImage.cpp
+++ ImageOutputting/ImageOutputting/outputImage.cpp
@@ -4,14 +4,17 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include <string>
 
 using namespace cv;
 using namespace std;
 
+
 int main()
 {
 	Mat image;
-	image = imread("C:\\Users\\mpkai\\Desktop\\Lena.png", IMREAD_ANYCOLOR); //HAVE TO ESCAPE THE BACKSLASHES
+	string path;
+	image = imread("x64\\Debug\\Lena.png", IMREAD_ANYCOLOR); //HAVE TO ESCAPE THE BACKSLASHES
 	namedWindow("Display Window", WINDOW_AUTOSIZE); //created window with name "Display Window" and its auto-sized
 	imshow("Display Window", image); //display the image to the window
 
